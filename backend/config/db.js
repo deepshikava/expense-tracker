4d9443cbb77f7be5b0410465e91e9ca953337ff0
JARVIS\deeps <deepshika2407@gmail.com>
2026-05-05 01:33:51 +0800

    Backend - env file changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const DB = process.env.MONGODB_URL;
+  const DB = process.env.MONGODB_URI;
 
   await mongoose.connect(DB).then(() => {
     console.log("Connected to MongoDB - DB Connected");
