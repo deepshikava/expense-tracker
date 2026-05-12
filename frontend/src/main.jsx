257ab00bb73971991c7f586850ed9efd709e1e7b
JARVIS\deeps <deepshika2407@gmail.com>
2026-05-12 13:20:03 +0800

    added vercel SpeedInsights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <SpeedInsights />
   </BrowserRouter>,
 );
